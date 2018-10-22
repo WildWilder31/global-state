b9c129bcac1eee0f4e7f98004169f055b2c6e7d6
Benoît Hubert <benoithubert@gmail.com>
2018-10-22 10:26:40 +0200

    Etat complété/non complété

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // props ou this.props ?
 // ça dépend du "type" (classe ou fonction) de composant
 // ici TodoItem reçoit ses props directement en paramètre
 // 
-const TodoItem = props => (
-    <div className="TodoItem">
-        <input type="checkbox" checked={props.completed} />
-        <span>{props.title}</span>
-    </div>
-);
+class TodoItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            completed: false
+        };
+    }
+
+    // Gestionnaire d'évènement = event handler
+    toggleCompleted = () => {
+        this.setState({
+            completed: !this.state.completed
+        });
+    }
+    render() {
+        return (
+            <div className="TodoItem">
+                <input
+                    type="checkbox"
+                    checked={this.state.completed}
+                    onChange={this.toggleCompleted}
+                />
+                <span>{this.props.title}</span>
+            </div>
+        );
+    }
+}
 
 export default TodoItem;
